3101d013a95e202bf8d0a1d4345b4b88e335e0e7
karl452 <karlastephaniee452@gmail.com>
2025-05-13 16:27:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#botao");
+botao.style.background="white"
+botao.addEventListener("click",e =>{ 
+    botao.style.background="white";
+    botao.innerHTML="Conhecer a si mesmo é o começo de toda sabedoria";
+});
+
